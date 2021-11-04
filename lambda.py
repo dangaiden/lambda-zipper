cd1df48edd3b556e600192b470eddd2b547a9ad2
dangaiden <dbalcaraz@gmail.com>
2021-11-04 20:12:33 +0100

    Working in presigned_url

--- lambda.py
+++ lambda.py
@@ -5,17 +5,14 @@ import boto3
 
 s3 = boto3.resource('s3')
 
-bucket_name = "tkw-priv"
-bucket_name_dest = "tkw-itgaiden-bucket"
-archive_name_dest = "images_new031121.zip"
-archive_public_access = "true"
+bucket_name_src = "tkw-priv"
+bucket_name_dst = "tkw-itgaiden-bucket"
+zip_name_dst = "images_041121.zip"
 
 # An object is a file 
 # and any metadata that describes that file
-s3_bucket = s3.Bucket(bucket_name)
+s3_bucket = s3.Bucket(bucket_name_src)
 summaries = s3_bucket.objects
-summaries_all = s3_bucket.objects.all()
-#s3_bucket.objects.pages()
 
 def count_objects(objects):
     count=0
@@ -32,14 +29,14 @@ def delete_object_if_exists(bucketName, key):
 def main_handler(event, context):
     try:
         print('Lambda starts:')
-    	
-        bucketNameSource = bucket_name
-        bucketNameDest = bucket_name_dest
-        archiveNameDest = archive_name_dest
         
+        # bucketNameSource = bucket_name
+        # bucketNameDest = bucket_name_dest
+        # archiveNameDest = archive_name_dest
+
         # Safe to delete
-        delete_object_if_exists(bucketNameDest, archiveNameDest)
-        print('Target zip:', bucketNameDest + '/' + archiveNameDest)
+        delete_object_if_exists(bucket_name_dst, zip_name_dst)
+        print('Target zip:', bucket_name_dst + '/' + zip_name_dst)
 
          # Get number of objects from bucket
         num_objects = count_objects(summaries)
@@ -51,15 +48,19 @@ def main_handler(event, context):
         # Gather the content from the body and parse it (JSON)
         body = (event['body'])
         dataDic = json.loads(body)
+        print("BEFORE LOOP")
         
+        zip_file = s3.ObjectSummary(bucket_name_dst, zip_name_dst)
+
+
         for item in dataDic['Images']:
             imgName = item['Name']
-            zip_file = s3.ObjectSummary(bucketNameDest, archiveNameDest)
+           
             print("zip_file:", zip_file)
-            print("Uploading files to ", archiveNameDest)
+            print("Uploading files to ", zip_name_dst)
             print("***************************************************")
             
-            obj = s3.Object(bucket_name, imgName)
+            obj = s3.Object(bucket_name_src, imgName)
             content = obj.get()['Body'].read()
             
             ## Open Zipfile and append each object,key (file)
@@ -73,8 +74,12 @@ def main_handler(event, context):
             # Set ACL for the ZIP as public-read
             print ("ZIP FILE FINISHED>>>>>", zip_file)
             zip_file.put(Body=zip_buffer.getvalue(), ACL='public-read')
-            
-        return "Download your images at: "+"'https://%s.s3.amazonaws.com/%s'" % (bucketNameDest, archiveNameDest)
+
+            #s3_client = boto3.client('s3')
+            #url = s3_client.generate_presigned_url(ClientMethod='get_object', Params={'Bucket': bucket_name_dst, 'Key': zip_file}, ExpiresIn=90)
+        #    print ("URL:", url)
+
+        return "Download your images at: "+"'https://%s.s3.amazonaws.com/%s'" % (bucket_name_dst, zip_name_dst)
     
     except Exception as err:
         return "Exception :" + str(err)
